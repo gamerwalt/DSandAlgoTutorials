8142db2eadf51dc28ba9b8a88b7418bdfd186226
Olawale Adewoyin <gamerwalt@gmail.com>
2020-05-12 06:20:33 -0400

    Initial checkin

---
+++ DataStructuresTutorials/LinkedList.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresTutorials
+{
+    public class LinkedList
+    {
+        private Node First;
+        private Node Last;
+
+        public void addFirst(int value)
+        {
+            var node = new Node(value);
+
+            if(First == null)
+            {
+                First = node;
+                Last = First;
+                return;
+            }
+
+            node.Next = First;
+            First = node;
+        }
+
+        public void addLast(int value)
+        {
+            var node = new Node(value);
+
+            if(First == null)
+            {
+                First = node;
+                Last = First;
+                return;
+            }
+
+            if(Last == First)
+            {
+                Last = node;
+                First.Next = Last;
+                return;
+            }
+
+            Last.Next = node;
+            Last = node;
+
+        }
+
+        public string Print()
+        {
+            var node = First;
+            StringBuilder sb = new StringBuilder();
+            int counter = 0;
+
+            while(node != null)
+            {
+                counter++;
+                if(counter == 1)
+                {
+                    sb.Append(node.Value);
+                }
+                else
+                {
+                    sb.Append(", " + node.Value);
+                }
+
+                node = node.Next;
+            }
+
+            return sb.ToString();
+        }
+
+        public void deleteFirst()
+        {
+            if(First == null)
+            {
+                return;
+            }
+
+            First = First.Next;
+        }
+
+        private class Node
+        {
+            public Node Next;
+            public int Value;
+
+            public Node(int value)
+            {
+                this.Value = value;
+            }
+        }
+    }
+}
---
+++ DataStructuresTutorialsTests/LinkedListTest.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructuresTutorials;
+using FluentAssertions;
+
+namespace DataStructuresTutorialsTests
+{
+    [TestClass]
+    public class LinkedListTest
+    {
+        [TestMethod]
+        public void should_be_able_to_add_to_the_front_of_a_linked_list()
+        {
+            var l = new LinkedList();
+
+            l.addFirst(10);
+            
+            var output = l.Print();
+            output.Should().Be("10");
+        }
+
+        [TestMethod]
+        public void should_be_able_to_add_to_the_last_of_a_linked_list()
+        {
+            var l = new LinkedList();
+            l.addFirst(10);
+            l.addLast(20);
+
+            var output = l.Print();
+            output.Should().Be("10, 20");
+        }
+
+        [TestMethod]
+        public void should_be_able_to_add_to_the_last_of_a_linked_list_if_we_have_more_items()
+        {
+            var l = new LinkedList();
+            l.addFirst(10);
+            l.addFirst(5);
+            l.addLast(20);
+
+            var output = l.Print();
+            output.Should().Be("5, 10, 20");
+        }
+
+        [TestMethod]
+        public void should_be_able_to_add_to_the_last_of_a_linked_list_if_we_have_more_and_more_items()
+        {
+            var l = new LinkedList();
+            l.addFirst(10);
+            l.addFirst(5);
+            l.addLast(15);
+            l.addLast(20);
+
+            var output = l.Print();
+            output.Should().Be("5, 10, 15, 20");
+        }
+
+        [TestMethod]
+        public void should_be_able_to_delete_the_first_node()
+        {
+            var l = new LinkedList();
+            l.addFirst(10);
+            l.addFirst(5);
+            l.addLast(15);
+            l.addLast(20);
+
+            l.deleteFirst();
+
+            var output = l.Print();
+            output.Should().Be("10, 15, 20");
+        }
+
+        [TestMethod]
+        public void should_be_able_to_delete_the_first_node_even_if_we_have_just_one_node()
+        {
+            var l = new LinkedList();
+            l.addFirst(10);
+
+            l.deleteFirst();
+
+            var output = l.Print();
+            output.Should().Be("");
+        }
+    }
+}
