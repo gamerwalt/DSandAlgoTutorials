f46bd499e8272b6c02b289bdcd840b3aa538801b
Olawale (Wally) Adewoyin <oadewoyin@obsglobal.com>
2020-05-23 00:32:49 -0400

    Started working on stacks

---
+++ DataStructuresTutorials/StringProblems.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresTutorials
+{
+    public class StringProblems
+    {
+        public string ReverseString(string stringToReverse)
+        {
+            if(stringToReverse == null)
+            {
+                throw new ArgumentException("The string to reverse cannot be null.");
+            }
+
+            Stack<char> letters = new Stack<char>();
+            for(int i = 0; i<stringToReverse.Length; i++)
+            {
+                letters.Push(stringToReverse[i]);
+            }
+
+            StringBuilder newString = new StringBuilder();
+            while(letters.Count != 0)
+            {
+                newString.Append(letters.Pop());
+            }
+
+            return newString.ToString();
+        }
+
+        public bool isBalancedParenthesis(string stringToCheck)
+        {
+            if(stringToCheck == null)
+            {
+                throw new ArgumentException("The string to check cannot be null.");
+            }
+
+            Stack<char> opposites = new Stack<char>();
+            for(int i = 0; i<stringToCheck.Length; i++)
+            {
+                var charToCheck = stringToCheck[i];
+                switch(charToCheck)
+                {
+                    case '(':
+                        opposites.Push(')');
+                        break;
+                    case '[':
+                        opposites.Push(']');
+                        break;
+                    case '<':
+                        opposites.Push('>');
+                        break;
+                    case ')':
+                        if (opposites.Count == 0) return false;
+                        if (!opposites.Pop().Equals(')'))
+                        {
+                            return false;
+                        }
+                        break;
+                    case ']':
+                        if (opposites.Count == 0) return false;
+                        if (!opposites.Pop().Equals(']'))
+                        {
+                            return false;
+                        }
+                        break;
+                    case '>':
+                        if (opposites.Count == 0) return false;
+                        if (!opposites.Pop().Equals('>'))
+                        {
+                            return false;
+                        }
+                        break;
+                }
+            }
+
+            //if we get here and the stack is empty, then we have a valid balanced string
+            return opposites.Count == 0;
+        }
+    }
+}
---
+++ DataStructuresTutorialsTests/StringProblemsTest.cs
@@ -0,0 +1,71 @@
+﻿using DataStructuresTutorials;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresTutorialsTests
+{
+    [TestClass]
+    public class StringProblemsTest
+    {
+        [TestMethod]
+        public void should_return_the_reversed_form_of_a_string()
+        {
+            var reverser = new StringProblems();
+            var reversedString = reverser.ReverseString("hello");
+
+            reversedString.Should().Equals("olleh");
+        }
+
+        [TestMethod]
+        public void should_return_an_empty_string_if_the_string_to_reverse_is_empty()
+        {
+            var reverser = new StringProblems();
+            var reversedString = reverser.ReverseString("");
+
+            reversedString.Should().Equals("");
+        }
+
+        [TestMethod]
+        public void should_return_true_if_the_string_is_balanced()
+        {
+            var balancer = new StringProblems();
+            var stringToCheck = "[<<>>([])]";
+            balancer.isBalancedParenthesis(stringToCheck).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void should_return_false_if_the_string_is_not_balanced()
+        {
+            var balancer = new StringProblems();
+            var stringToCheck = "[<<>>([))]";
+            balancer.isBalancedParenthesis(stringToCheck).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void should_return_false_if_the_string_is_not_balanced_because_of_a_bracket_that_wasnt_closed()
+        {
+            var balancer = new StringProblems();
+            var stringToCheck = "<[<<>>([))]"; //needs the end to be a >
+            balancer.isBalancedParenthesis(stringToCheck).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void should_return_false_if_the_string_is_not_balanced_because_the_opposites_are_used()
+        {
+            var balancer = new StringProblems();
+            var stringToCheck = ")("; //needs the end to be a >
+            balancer.isBalancedParenthesis(stringToCheck).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void should_return_true_if_the_string_is_empty()
+        {
+            var balancer = new StringProblems();
+            var stringToCheck = "";
+            balancer.isBalancedParenthesis(stringToCheck).Should().BeTrue();
+        }
+    }
+}
