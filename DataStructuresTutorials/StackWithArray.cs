2a90db7808f4d352c6a0766bdca0801176272c31
Olawale (Wally) Adewoyin <oadewoyin@obsglobal.com>
2020-05-24 01:50:38 -0400

    build a stack with an array of ints

---
+++ DataStructuresTutorials/StackWithArray.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresTutorials
+{
+    public class StackWithArray
+    {
+        int originalCapacity;
+        int[] items;
+        int count;
+
+        public StackWithArray(int originalCapacity)
+        {
+            this.originalCapacity = originalCapacity;
+            items = new int[this.originalCapacity];
+        }
+
+        public void Push(int item)
+        {
+            if(count == this.originalCapacity)
+            {
+                int[] newItems = new int[this.originalCapacity * 2];
+                Array.Copy(items, newItems, this.originalCapacity);
+                
+                this.originalCapacity = this.originalCapacity * 2;
+                
+                items = newItems;
+            }
+            
+            items[count] = item;
+            count++;
+        }
+
+        public int Pop()
+        {
+            if(count == 0)
+            {
+                throw new Exception("The stack is empty.");
+            }
+
+            var item = items[count-1];
+            items[count - 1] = 0;
+            count--;
+
+            return item;
+        }
+
+        public int Peek()
+        {
+            if (count == 0)
+            {
+                throw new Exception("The stack is empty.");
+            }
+
+            return items[count - 1];
+        }
+
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder listOfItems = new StringBuilder();
+            for(int i = 0; i<count; i++)
+            {
+                listOfItems.Append(items[i]);
+            }
+
+            return listOfItems.ToString();
+        }
+    }
+}
---
+++ DataStructuresTutorialsTests/StackWithArrayTests.cs
@@ -0,0 +1,94 @@
+﻿using DataStructuresTutorials;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresTutorialsTests
+{
+    [TestClass]
+    public class StackWithArrayTests
+    {
+        [TestMethod]
+        public void should_be_able_to_add_to_the_stack()
+        {
+            var stack = new StackWithArray(4);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            var result = stack.ToString();
+            result.Should().Be("1234");
+        }
+
+        [TestMethod]
+        public void should_be_able_to_add_to_the_stack_even_if_it_gets_full_of_the_original_capacity()
+        {
+            var stack = new StackWithArray(4);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+
+            var result = stack.ToString();
+            result.Should().Be("123456");
+        }
+
+        [TestMethod]
+        public void should_be_able_to_remove_an_item_from_the_stack()
+        {
+            var stack = new StackWithArray(4);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            var poppedItem = stack.Pop();
+            
+            var result = stack.ToString();
+            
+            result.Should().Be("123");
+            poppedItem.Should().Be(4);
+        }
+
+        [TestMethod]
+        public void should_be_able_to_remove_an_item_from_the_stack_twice()
+        {
+            var stack = new StackWithArray(4);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            var poppedItem1 = stack.Pop();
+            var poppedItem2 = stack.Pop();
+
+            var result = stack.ToString();
+
+            result.Should().Be("12");
+            poppedItem1.Should().Be(4);
+            poppedItem2.Should().Be(3);
+        }
+
+        [TestMethod]
+        public void should_be_able_to_peek_at_an_item_in_the_stack()
+        {
+            var stack = new StackWithArray(4);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            var peekedItem = stack.Peek();
+
+            var result = stack.ToString();
+
+            result.Should().Be("1234");
+            peekedItem.Should().Be(4);
+        }
+    }
+}
